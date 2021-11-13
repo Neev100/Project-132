2a13e5c25238ec4e495112b91400191b8fc70c95
Neev100 <79758801+Neev100@users.noreply.github.com>
2021-11-13 15:21:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+img = "";
+status = "";
+
+function preload() {
+    img = loadImage("bedroom.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "status - Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error();
+    }
+    else {
+        console.log(results);
+    }
+}
